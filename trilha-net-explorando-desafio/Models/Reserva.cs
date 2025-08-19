80ad6ded1ad524ab36ab77fc20429ed89dc89961
Lauren Maciel <laurenmaciel1@gmail.com>
2025-08-19 01:36:22 -0300

    Desafio concluido

---
+++ trilha-net-explorando-desafio/Models/Reserva.cs
@@ -0,0 +1,64 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        private const decimal DescontoPercentual = 0.10m;
+        private const int DiasParaDesconto = 10;
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // *IMPLEMENTE AQUI*
+            if (Suite != null && hospedes != null && hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new Exception("A quantidade de hospedes execede a capacidade da suite");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            return Hospedes != null ? Hospedes.Count : 0;
+            return 0;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            decimal valor = 0;
+            valor = DiasReservados * Suite.ValorDiaria;
+
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI*
+            if (DiasReservados >= DiasParaDesconto)
+            {
+                valor -= valor * DescontoPercentual;
+                //valor = 0;
+            }
+
+            return valor;
+        }
+    }
+}
---
+++ trilha-net-explorando-desafio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using DesafioProjetoHospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Luiza");
+Pessoa p2 = new Pessoa(nome: "Rodolfo");
+//Pessoa p3 = new Pessoa(nome: "Rosa", "Holloway");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+//hospedes.Add(p3);
+
+// Cria a suíte
+Suite suite = new Suite(tipoSuite: "Luxo", capacidade: 2, valorDiaria: 195);
+
+// Cria uma nova reserva, passando a suíte e os hóspedes
+Reserva reserva = new Reserva(diasReservados: 10);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+// Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine("╔════════════════════════════════════════╗");
+Console.WriteLine($"║Hóspedes: {reserva.ObterQuantidadeHospedes(),-30}║");
+Console.WriteLine($"║Valor diária: {reserva.CalcularValorDiaria(),-26:F2}║");
+Console.WriteLine("╠════════════════════════════════════════╣");
+Console.WriteLine($"║Suíte {reserva.Suite.TipoSuite,-34}║");
+foreach (var hospede in reserva.Hospedes)
+{
+    Console.WriteLine($"║ • {hospede.NomeCompleto, -37}║");
+}
+Console.WriteLine("╚════════════════════════════════════════╝");
